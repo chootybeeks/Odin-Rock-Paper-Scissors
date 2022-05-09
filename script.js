cbeb02140d09afa2a2338afc7745cc61814a3506
chootybeeks <quadi.d.banks@gmail.com>
2022-05-09 14:22:15 -0400

    Finish implementation of computerPlay function

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function computerPlay {
+function computerPlay() {
+    //This variable contains the maximum value that the randomNumber will generate.
     const max = 9;
     
-    let guess;
+    let computerSelection;
     let randomNumber;
-
+    //Generate a random number between 1 and max (9)
     randomNumber = Math.floor(Math.random() * max) + 1;
-}
+    
+    //If the random number is 1 - 3, the guess is Rock
+    if (randomNumber < 4) {
+        return computerSelection = "Rock";
+    }
+    //If the random number is 4 - 6, the guess is Paper
+    else if (randomNumber >= 4 || randomNumber <= 6) {
+    return computerSelection = "Paper";
+    }
+    //If the random number is 7 - 9. the guess is Scissors
+    else if (randomNumber > 6 || randomNumber <= 9) {
+    return computerSelection = "Scissors";
+    }
+    }
