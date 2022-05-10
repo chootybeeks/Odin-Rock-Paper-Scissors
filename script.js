81444f8662019de213e58441701ed09aab95864e
chootybeeks <quadi.d.banks@gmail.com>
2022-05-10 10:45:01 -0400

    Remove userPlay() function, add computerScore and playerScore as global variables

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 function computerPlay() {
-    //This variable contains the maximum value that the randomNumber will generate.
-    const max = 3;
-    
-    let computerSelection;
-    let randomNumber;
-    //Generate a random number between 1 and max (3)
-    randomNumber = Math.floor(Math.random() * max) + 1;
-    
-    //If the random number is 1 computerSelection is Rock
-    if (randomNumber == 1) {
-        return computerSelection = "rock";
-    }
-    //If the random number is 2 computerSelection is Paper
-    else if (randomNumber == 2) {
-    return computerSelection = "paper";
-    }
-    //If the random number 3 computerSelection is Scissors
-    else if (randomNumber == 3) {
-    return computerSelection = "scissors";
-    }
-    }
+//This variable contains the maximum value that the randomNumber will generate.
+const max = 3;
 
-function userPlay() {
-let playerSelection = prompt("Rock, Paper, or Scissors?: ").toLowerCase();
-return playerSelection;
+let computerSelection;
+let randomNumber;
+//Generate a random number between 1 and max (3)
+randomNumber = Math.floor(Math.random() * max) + 1;
+
+//If the random number is 1 computerSelection is Rock
+if (randomNumber == 1) {
+return computerSelection = "rock";
+}
+//If the random number is 2 computerSelection is Paper
+else if (randomNumber == 2) {
+return computerSelection = "paper";
+}
+//If the random number 3 computerSelection is Scissors
+else if (randomNumber == 3) {
+return computerSelection = "scissors";
+}
 }
 
+let computerScore, playerScore;
 function playRound(playerSelection, computerSelection) {
 //Rock beats scissors, scissors beats paper, and paper beats rock. If both are the same, then there is a draw.
 let result;
