3bb1e14379a57e3d971faf8ad55ca72e5b09c612
chootybeeks <quadi.d.banks@gmail.com>
2022-05-17 18:21:45 -0400

    Remove old code for refactoring

--- script.js
+++ script.js
@@ -8,24 +8,6 @@ rock.addEventListener('click', newPlayRound);
 paper.addEventListener('click', newPlayRound);
 scissors.addEventListener('click', newPlayRound);
 
-//Rewrite playRound function to accept (event) as an argument
-function newPlayRound(event, scoreArray) {
-    //Declare variables to keep track of
-    let playerScore;
-    let computerScore;
-    //let roundNumber = 0;
-    let results;
-
-    //Declare and initialize player and computer selections
-    let playerSelection = event.target.getAttribute('id');
-    let computerSelection = computerPlay();
-    results = playRound(playerSelection, computerSelection);
-    console.log(results);
-    
-    
-    
-}
-
 function computerPlay() {
 //This variable contains the maximum value that the randomNumber will generate.
 const max = 3;
@@ -48,59 +30,3 @@ else if (randomNumber == 3) {
 return computerSelection = "scissors";
 }
 }
-
-function playRound(playerSelection, computerSelection) {
-    let playerScore = 0;
-    let computerScore = 0;
-    let roundNumber = 1;
-   
-//Rock beats scissors, scissors beats paper, and paper beats rock. If both are the same, then there is a draw.
-while (roundNumber <= 5) {
-    if (playerSelection == "rock" && computerSelection == "scissors") {
-        playerScore++;
-    }
-        else if (playerSelection == "rock" && computerSelection == "paper") {
-            computerScore++;
-        }
-        else if (playerSelection == "paper" && computerSelection == "scissors") {
-            computerScore++;
-        }
-        else if (playerSelection == "paper" && computerSelection == "rock") {
-            playerScore++;
-        }
-        else if (playerSelection == "scissors" && computerSelection == "paper") {
-            playerScore++;
-        }
-        else if (playerSelection == "scissors" && computerSelection == "rock") {
-            computerScore++;
-        }
-    roundNumber++;    
-    }
-    return [playerScore, computerScore];
-}   
-    
-//function game() {
-//For loop will all the game to be played a pre-determined number of times, in this case 5 times.
-//let numberOfGames = 5;
-
-/*for (let i = 0; i < numberOfGames; i++) {
-    
-    let playerSelection = prompt("Rock, Paper, or Scissors?: ");
-    playerSelection = playerSelection.toLowerCase();
-    let computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-}*/
-//If statement to determine the winner, if there is one
-/*if (playerScore > computerScore) {
-    console.log(`You beat the computer! Final score: ${playerScore} to ${computerScore}`);
-}
-else if (playerScore < computerScore) {
-    console.log(`The computer beat you! Final score: ${computerScore} to ${playerScore}`);
-}
-else console.log(`Draw. Final score: ${playerScore} to ${computerScore}`);
-}*/
-
-//game();
-
-
-//});
