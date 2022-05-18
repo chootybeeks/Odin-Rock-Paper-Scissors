d773e0bb677aa5064f54b615c6a06c47cd0c8576
chootybeeks <quadi.d.banks@gmail.com>
2022-05-18 14:19:37 -0400

    Begin implementing game() and playRound functions

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-//Add querySelector for each button
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
-
-//Add eventListener and console log for each button click
-rock.addEventListener('click', newPlayRound);
-paper.addEventListener('click', newPlayRound);
-scissors.addEventListener('click', newPlayRound);
-
-function computerPlay() {
-//This variable contains the maximum value that the randomNumber will generate.
-const max = 3;
-
-let computerSelection;
-let randomNumber;
-//Generate a random number between 1 and max (3)
-randomNumber = Math.floor(Math.random() * max) + 1;
-
-//If the random number is 1 computerSelection is Rock
-if (randomNumber == 1) {
-return computerSelection = "rock";
+//game() will contain all logic in the game
+function game() {
+    let playerScore;
+    let computerScore;
+    let roundNumber;
 }
-//If the random number is 2 computerSelection is Paper
-else if (randomNumber == 2) {
-return computerSelection = "paper";
+
+//playRound() with event listeners
+function playRound() {
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
+//Arrays for playerOptions and computerOptions
+const playerChoices = [rockButton, paperButton, scissorsButton];
+const computerChoices = ['rock', 'paper', 'scissors'];
+
+playerChoices.forEach(choice => {
+    choice.addEventListener('click', () => {
+        const currentRound = document.querySelector('.round-counter')
+        roundNumber++
+        currentRound.innerText = `Rounds Left ${5 - roundNumber}`
+
+        //Choose a random number between 0 and 2
+        const randomNumber = Math.floor(Math.random() * 3)
+        const computerPlay = computerChoices[randomNumber]
+    })
+})
 }
-//If the random number 3 computerSelection is Scissors
-else if (randomNumber == 3) {
-return computerSelection = "scissors";
-}
-}
+
+
+
