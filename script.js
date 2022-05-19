dc0e8f2dfc6c9fb32bbb0a89d9cab1522e0ef3e1
chootybeeks <quadi.d.banks@gmail.com>
2022-05-19 13:24:26 -0400

    Create userChoiceDisplay and ComputerChoiceDisplay elements

---
+++ script.js
@@ -0,0 +1,4 @@
+//Create h1 elements to display the userChoice and computerChoice
+const userChoiceDisplay = document.createElement('h1');
+const computerChoiceDisplay = document.createElement('h1')
+
