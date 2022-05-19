8b573beea0e1dcf64ec022c1075f909cc0bb88f9
chootybeeks <quadi.d.banks@gmail.com>
2022-05-19 13:31:34 -0400

    Create resultDisplayElement and rename previous variables

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //Create h1 elements to display the userChoice and computerChoice
-const userChoiceDisplay = document.createElement('h1');
-const computerChoiceDisplay = document.createElement('h1')
+const userChoiceDisplayElement = document.createElement('h1');
+const computerChoiceDisplayElement = document.createElement('h1')
 
+//Create element to display the result of the round
+const resultDisplayElement = document.createElement('h1')
+
+//The array of choices that the computer and user will choose from
+const choicesArray = ['rock','paper','scissors']
